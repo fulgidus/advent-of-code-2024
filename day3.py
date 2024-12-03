9601633e125709c96294d9da6e0c9d34c5add2fb
Alessio Corsi <alessio.corsi@gmail.com>
2024-12-03 13:18:44 +0100

    Day3 part2

--- day3.py
+++ day3.py
@@ -1,10 +1,10 @@
 import re
 
-def extract_and_multiply(text):
+def process_instructions(text):
     pattern = re.compile(r'''
-        (?P<do>do\(\))             |
-        (?P<dont>don't\(\))        |
-        (?P<mul>mul\((\d+),(\d+)\))
+        (?P<do>do\(\))                      |
+        (?P<dont>don't\(\))                 |
+        (?P<mul>mul\((?P<x>\d+),(?P<y>\d+)\))
     ''', re.VERBOSE)
     total = 0
     enabled = True
@@ -14,14 +14,26 @@ def extract_and_multiply(text):
         elif match.group('dont'):
             enabled = False
         elif match.group('mul') and enabled:
-            x = int(match.group(2))
-            y = int(match.group(3))
+            x = int(match.group('x'))
+            y = int(match.group('y'))
             total += x * y
+    return total
+
+def extract_and_multiply(text):
+    return process_instructions(text)
+
+def extract_and_multiply_all(text):
+    pattern = re.compile(r'mul\((\d+),(\d+)\)')
+    matches = pattern.findall(text)
+    total = sum(int(x) * int(y) for x, y in matches)
     return total
 
 # Esempio di utilizzo
 with open('day3.txt', 'r') as file:
     data = file.read()
 
-result = extract_and_multiply(data)
-print("Result: ", result)
+result_part1 = extract_and_multiply_all(data)
+print("Result Part 1: ", result_part1)
+
+result_part2 = process_instructions(data)
+print("Result Part 2: ", result_part2)
